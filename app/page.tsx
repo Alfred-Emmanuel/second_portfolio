9bb0b8029fc0080b2a77604f6e973d85bbeb4953
Alfred Emmanuel <ealfred828@gmail.com>
2024-01-17 12:00:04 +0100

    left the hero section halfway done because i need to work on the navbar first

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,15 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <section className='flex h-screen'>
+      <section className='flex h-screen px-6 md:px-0 bg-secondary'>
         <div className='hidden md:block bg-secondary w-[32%]'></div>
         <div className='bg-secondary w-full md:bg-primary md:w-[68%]'>
-          
+          <div>
+            <h5>Hi, I'm Alfred</h5>
+            <h1>
+              I like making fun, interactive things with code. I also talk & write about those things.
+            </h1>
+          </div>
         </div>
       </section>
     </main>

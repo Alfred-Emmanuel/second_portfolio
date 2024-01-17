9070fc5ba28bfe5e42b2ecce9d325ad5decabcd5
Alfred Emmanuel <ealfred828@gmail.com>
2024-01-17 11:47:22 +0100

    working on the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,10 @@ export default function Home() {
   return (
     <main>
       <section className='flex h-screen'>
-        <div className='bg-secondary w-[32%]'></div>
-        <div className='bg-primary w-[68%]'></div>
+        <div className='hidden md:block bg-secondary w-[32%]'></div>
+        <div className='bg-secondary w-full md:bg-primary md:w-[68%]'>
+          
+        </div>
       </section>
     </main>
   )

ce3d1f17af064b5481cc607d3a82265b1b5d829b
Alfred Emmanuel <ealfred828@gmail.com>
2024-01-17 11:39:49 +0100

    changed the color codes

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <section className=''>
-
+      <section className='flex h-screen'>
+        <div className='bg-secondary w-[32%]'></div>
+        <div className='bg-primary w-[68%]'></div>
       </section>
     </main>
   )

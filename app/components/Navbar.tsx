c81d72b60479428e6471b08d830f9fe88439587a
Alfred Emmanuel <ealfred828@gmail.com>
2024-01-19 19:02:48 +0100

    working on the navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Borel } from 'next/font/google'
+
+const borel = Borel({ 
+  weight: '400',
+  subsets: ['latin'],
+})
+
+export default function Navbar() {
+  return (
+    <nav className='flex justify-between items-center text-[#9A3B3B] absolute top-12 left-0 px-6 md:px-32 w-full'>
+        <div className={borel.className}>
+            <h1 className="text-4xl font-black">alfred</h1>
+        </div>
+        <span>A</span>
+    </nav>
+  )
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,39 @@
 import Image from 'next/image'
+import Navbar from './components/Navbar'
+import { Borel, Archivo_Black } from 'next/font/google'
+import { arch } from 'os'
+// import { Lemon } from 'next/font/google'
+
+const archivo_Black = Archivo_Black({ 
+  weight: '400',
+  subsets: ['latin'],
+})
+
+const borel = Borel({ 
+  weight: '400',
+  subsets: ['latin'],
+})
+
 
 export default function Home() {
   return (
-    <main>
-      <section className='flex h-screen px-6 md:px-0 bg-secondary'>
-        <div className='hidden md:block bg-secondary w-[32%]'></div>
-        <div className='bg-secondary w-full md:bg-primary md:w-[68%]'>
-          <div>
-            <h5>Hi, I'm Alfred</h5>
-            <h1>
-              I like making fun, interactive things with code. I also talk & write about those things.
-            </h1>
+    <main className='text-white'>
+      <Navbar />
+      <section className='flex h-screen'>
+          <div className='hidden md:block bg-secondary md:w-[32%]'></div>
+          <div className='bg-secondary w-full md:bg-primary md:w-[68%] px-6 pt-44 md:pt-20'>
+            <div>
+              <h5 className='text-[0.95rem] mb-2'>👋 <span className={borel.className}>Hi, I'm Alfred</span></h5>
+              <div className={archivo_Black.className}>
+                <h1 className='text-[1.9rem] font-bold leading-20'>
+                  I like making fun, interactive things with code. I also talk & write about those things.
+                </h1>
+              </div>
+            </div>
           </div>
-        </div>
+      </section>
+      <section className='bg-[#9A3B3B] h-screen'>
+
       </section>
     </main>
   )
